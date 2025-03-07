0e69af6d43cd37828200cdb8e8ddf7b817d02686
diana <dpaez@serempre.com>
2025-03-06 21:56:57 -0500

    Add user Crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
@@ -10,31 +10,39 @@ export class UserController {
 
   @Post()
   @ApiOperation({ summary: 'Create user' })
-  create(@Body() createUser: CreateUserDto) {
+  async create(@Body() createUser: CreateUserDto) {
     return this.userService.create(createUser);
   }
 
   @Get()
   @ApiOperation({ summary: 'Get all'})
-  findAll() {
+  async findAll() {
     return this.userService.findAll();
+  }
+
+  @Get("/table")
+  @ApiOperation({ summary: 'Get all width page'})
+  @ApiQuery({ name: 'page', required: false, type: Number, example: 1 })
+  @ApiQuery({ name: 'limit', required: false, type: Number, example: 10 })
+  async  findAllForTable(@Query('page') page: number, @Query('limit') limit: number) {
+    return this.userService.findAllForTable(Number(page), Number(limit));
   }
 
   @Get(':id')
   @ApiOperation({ summary: 'Get one' })
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
   }
 
   @Patch(':id')
   @ApiOperation({ summary: 'Update one' })
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   @ApiOperation({ summary: 'Delete one' })
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return this.userService.remove(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,25 +8,59 @@ export class UserService {
 
   constructor(private prisma: PrismaService) {}
 
-  create(user: CreateUserDto) {
-    return this.prisma.user.create({
+  async create(user: CreateUserDto) {
+
+    const userCreated =await this.prisma.user.create({
       data: user
     });
+    return userCreated
   }
 
-  findAll() {
-    return this.prisma.user.findMany();
+  async findAll() {
+    const users=await this.prisma.user.findMany();
+    return users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findAllForTable(page:number,limit:number) {
+    const skip = (page - 1) * limit;
+
+    const users = await this.prisma.user.findMany({
+      skip,
+      take: limit,
+    });
+
+    const total = await this.prisma.user.count();
+
+     return {
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit),
+      data: users,
+    };
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOne(id: string) {
+    const userFound = await this.prisma.user.findUnique({
+      where: { id : id } 
+  })
+    return userFound
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const userUpdated= await this.prisma.user.update({
+      where: { id: id },
+      data: updateUserDto
+    })
+    return userUpdated;
+  }
+
+  async remove(id: string) {
+    
+    const userDeleted = await  this.prisma.user.delete({
+      where: { id: id }
+    });
+
+    return userDeleted
   }
 }
